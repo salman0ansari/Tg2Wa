a50a94246680cab44d1b0963799727e4d954ff15
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-07-13 16:56:59 +0530

    feat: add baileys module

---
+++ core/connect.js
@@ -0,0 +1,29 @@
+const {
+    WAConnection,
+    Browsers
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+// const qrcode = require('qrcode-terminal')
+
+const conn = new WAConnection()
+
+exports.Whatsapp = conn
+
+exports.connect = async () => {
+
+    // Custom browser
+    conn.browserDescription = Browsers.macOS('Chrome')
+    
+    conn.on('qr', async (qr) => {
+        console.log('\033[1;32mScan the QR code above.\x1b[0m')
+    })
+    fs.existsSync('./userData.json') && conn.loadAuthInfo('./userData.json')
+
+    await conn.connect({ timeoutMs: 3*1000 })
+    fs.writeFileSync('./userData.json', JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
+    console.log('='.repeat(50))
+    console.log(`| + WA Version: ${conn.user.phone.wa_version}`)
+    console.log(`| + Device: ${conn.user.phone.device_manufacturer}`)
+    console.log('='.repeat(50))
+    return conn
+}
---
+++ core/helper.js
@@ -0,0 +1,31 @@
+const con = require('./connect')
+const {
+    Mimetype,
+    MessageType
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+const wa = con.Whatsapp
+
+exports.sendText = function (jid, text) {
+    wa.sendMessage(jid, text, MessageType.text)
+}
+
+exports.sendSticker = function (jid, url) {
+    wa.sendMessage(
+        jid + '@s.whatsapp.net',
+        { url: url },
+        MessageType.sticker, { mimetype: Mimetype.sticker }
+    )
+}
+
+exports.sendImage = async(from, buffer, caption = "") => {
+    await wa.sendMessage(from, buffer, MessageType.image, { caption: caption })
+}
+
+exports.sendGif = (from, gif) => {
+	wa.sendMessage(from, gif, MessageType.video, {mimetype: "video/gif"})
+}
+
+
+//incomplete
+// exports.sendMediaURL = async(to, 
