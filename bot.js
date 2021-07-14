5cab31e973c6fbb0d4f283311fed9035d36ffc5a
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-07-14 11:10:07 +0530

    fix: PromiseRejectionWarning(will fix code later -_-)

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const limitConfig = {
     window: 5000,
     limit: 5,
     onLimitExceeded: async (ctx, next) => {
-        await ctx.reply('rate limit exceeded Deleting...')
+        await ctx.reply('rate limit exceeded Deleting...').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
         await ctx.deleteMessage()
     }
 }
@@ -30,28 +30,28 @@ const KEY = process.env.KEY // invitation code
 const Tg2Wa = new Scenes.WizardScene(
     'TelegramToWhatsapp',
     async (ctx) => {
-        await ctx.reply(`Send invitation code to access this bot! :)`)
+        await ctx.reply(`Send invitation code to access this bot! :)`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
         return ctx.wizard.next();
     },
     async (ctx) => {
         ctx.wizard.state.key = ctx.message.text;
         if (ctx.wizard.state.key === KEY) {
-            await ctx.reply('Success !!')
-            await ctx.reply('Hit /continue')
+            await ctx.reply('Success !!').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
+            await ctx.reply('Hit /continue').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             return ctx.wizard.next();
         }
         else {
-            await ctx.reply(`Invalid invitation code\nStart again ?? /setup`);
+            await ctx.reply(`Invalid invitation code\nStart again ?? /setup`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             return ctx.scene.leave();
         }
     },
     async (ctx) => {
-        await ctx.reply("Enter your name")
+        await ctx.reply("Enter your name").then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
         return ctx.wizard.next();
     },
     async (ctx) => {
         ctx.wizard.state.name = ctx.message.text;
-        await ctx.reply(`Enter your mobile no in international format\ne.g. +919876543210`)
+        await ctx.reply(`Enter your mobile no in international format\ne.g. +919876543210`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
         return ctx.wizard.next();
     },
     async (ctx) => {
@@ -60,34 +60,34 @@ const Tg2Wa = new Scenes.WizardScene(
             const { name, mobile } = ctx.wizard.state;
         if (!mobile.includes('+91') && !Number.isInteger(mobile)) {
             // if(isValidPhone(mobile)
-            await ctx.reply(`Not an Indian Number Or\nInvalid Format\ne.g. +919876543210 `);
+            await ctx.reply(`Not an Indian Number Or\nInvalid Format\ne.g. +919876543210 `).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             return ctx.scene.leave();}
         else{
-            await ctx.reply(`Your name: ${name}\nYour Number: ${mobile}`);
+            await ctx.reply(`Your name: ${name}\nYour Number: ${mobile}`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             await ctx.tg.sendMessage(ADMIN, `Name: ${name}\nMobile Number: ${mobile}\nUser chat: ${JSON.stringify(ctx.chat)}`)
             let alreadyThere = await User.findOne({ userid: JSON.stringify(ctx.chat.id) }).exec();
             if (alreadyThere) {
-                ctx.reply('Welcome Back');
+                ctx.reply('Welcome Back').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
                     return ctx.scene.leave();
                 }
-                await ctx.reply('Saving your number to db...');
+                await ctx.reply('Saving your number to db...').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
                 const added = await new User({ userid: JSON.stringify(ctx.chat.id), name: name, phone: mobile });
                 added.save(function (err) {
                     if (err) return handleError(err);
                     // saved!
                 });
                 if (added) {
-                    ctx.reply('Done')
-                    ctx.reply('Now you can start sending me stickers')
+                    ctx.reply('Done').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
+                    ctx.reply('Now you can start sending me stickers').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
                 } else {
-                    ctx.reply('Something Wrong')
+                    ctx.reply('Something Wrong').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
                 }
                 return ctx.scene.leave();
         }
     
         
         } catch (error) {
-            ctx.reply('Error')
+            console.log('Error')
         }
         
         
@@ -99,7 +99,7 @@ const stage = new Scenes.Stage([Tg2Wa])
 bot.use(stage.middleware())
 
 bot.command('start', (ctx) => {
-    ctx.reply(`Hello \nStart Setup: /setup \nUpdate Number /update\nInfo: /me \nKey:`);
+    ctx.reply(`Hello \nStart Setup: /setup \nUpdate Number /update\nInfo: /me \nKey:`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
 });
 
 bot.command('setup', (ctx) => {
@@ -110,31 +110,31 @@ bot.command('setup', (ctx) => {
 bot.command('me', async (ctx) => {
     try {
         const { userid, name, phone } = await User.findOne({ userid: JSON.stringify(ctx.chat.id) }).exec();
-        ctx.reply(`Your name: ${name}\nYour Number: ${phone}`);
+        ctx.reply(`Your name: ${name}\nYour Number: ${phone}`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     } catch (error) {
-        ctx.reply(`Error`);
+        ctx.reply(`Error`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     }
 
 })
 
 bot.command('update', async (ctx) => {
-    ctx.reply('Enter new number');
+    ctx.reply('Enter new number').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     try {
         bot.on('message', async (ctx) => {
             const newNum = ctx.message.text
             if (!newNum.includes('+91') && !Number.isInteger(newNum)) {
-                await ctx.reply(`Not an Indian Number Or\nInvalid Format\ne.g. +919876543210 `);
+                await ctx.reply(`Not an Indian Number Or\nInvalid Format\ne.g. +919876543210 `).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             }
             else {
-                ctx.reply(`Updating...`);
+                ctx.reply(`Updating...`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
                 const filter = { userid: JSON.stringify(ctx.chat.id) }
                 const update = { phone: newNum }
                 const updateNum = await User.findOneAndUpdate(filter, update);
-                await ctx.reply(`Updated Check Here: /me`);
+                await ctx.reply(`Updated Check Here: /me`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
             }
         })
     } catch (error) {
-        ctx.reply(`Error`);
+        ctx.reply(`Error`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     }
 })
 
@@ -143,7 +143,7 @@ bot.on('sticker', async (ctx) => {
     const {phone} = await User.findOne({ userid: JSON.stringify(ctx.chat.id) }).exec();
     let { sticker } = await ctx.message
     if (sticker.is_animated) {
-        return ctx.reply('Sorry currently i dont support animated sticker')
+        return ctx.reply('Sorry currently i dont support animated sticker').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     }
     else {
         let stickerId = await sticker.file_id
@@ -152,12 +152,12 @@ bot.on('sticker', async (ctx) => {
         wa.sendSticker(phone, href)
     }
     } catch (error) {
-        ctx.reply('Error: Run /setup again')
+        ctx.reply('Error: Run /setup again').then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
     }  
 })
 
 bot.command('help', async (ctx) => {
-    ctx.reply(`Start Setup: /setup \nUpdate Number /update\nInfo: /me \nKey:`);
+    ctx.reply(`Start Setup: /setup \nUpdate Number /update\nInfo: /me \nKey:`).then(console.log('Done')).catch((e) => console.log('Bot Blocked'))
 })
 
 // bot.command('/ban
